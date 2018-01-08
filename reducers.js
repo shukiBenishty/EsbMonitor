a10b27bf9fe8bc56b78947ecd005eb9c1379ebd3
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2018-01-08 14:18:33 +0200

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -1,24 +1,39 @@
+// @flow
 import _ from 'lodash';
 
 const INITIAL_STATE = {
+  storyId: '',
   eventId: 0,
-  issued: Date()
+  issued: Date(),
+  status: '',
+  filterValue: ''
 };
 
 const reducers = (state = INITIAL_STATE, action) => {
 
   switch( action.type ) {
 
-    case 'NEW_EVENT':
+    case 'NEW_EVENT': {
       state = _.assign({}, state, {
-                                      eventId: action.data.eventId,
-                                      issued: new Date(action.data.issued)
-                                  });
+                                    storyId: action.data.storyId,
+                                    eventId: action.data.eventId,
+                                    issued: new Date(action.data.issued),
+                                    status: action.data.status
+                                 })
+    }
+    break;
+
+    case 'FILTER_CHANGED': {
+      state = _.assign({}, state, {
+                                    filterValue: action.data.filterValue
+                                 })
+    }
+    break;
 
   }
 
   return state;
 
-}
+};
 
 export default reducers;
