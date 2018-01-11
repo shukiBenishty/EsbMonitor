9de981139d0056622cba0a6fe8e8f6a57fc8b971
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-11 13:00:19 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var cssName = process.env.NODE_ENV === 'production' ? 'styles-[hash].css' : 'styles.css';
+
 var jsName = 'bundle.js';
 
 var BUILD_DIR = path.resolve(__dirname, 'assets');
@@ -29,21 +31,34 @@ var config = {
          loader: ExtractTextPlugin.extract({
               loader: 'css-loader',
               query: {
-                modules: false
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                modules: true
               }
             })
       },
+      { test: /\.(woff|woff2|ttt|eot|otf)/, loader: 'url-loader?limit=1' },
+      {
+        test: /\.png/,
+        //loader: 'url-loader?limit=10000&mimetype=image/png',
+        loader: 'file-loader',
+        query: {
+          name: 'images/[hash].[ext]',
+          publicPath: '/'
+        }
+      },
+      { test: /\.svg/, loader: 'url-loader?limit=26000&mimetype=svg+xml' }
     ]
   },
-  node: {
-   fs: "empty"
+  devServer: {
+      headers: { 'Access-Control-Origin': '*'}
   },
- plugins: [
-   new ExtractTextPlugin({
-     filename: 'styles.css',
-     allChunks: true
-   })
- ]
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      allChunks: true
+    })
+  ]
+
 };
 
 module.exports = config;
