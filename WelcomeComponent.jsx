c6136fbae3f62c80dce79e9dac3c160a189c09ba
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-11 12:59:55 +0200

    Update WelcomeComponent.jsx

--- WelcomeComponent.jsx
+++ WelcomeComponent.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {
   userName: string
@@ -11,18 +12,30 @@ class WelcomeComponent extends React.Component<Props> {
     super(props);
 
     this.styles = {
+
+      navigation: {
+        height: "12%"
+      },
       container: {
         display: "flex",
         flexDirection: "row-reverse",
         backgroundColor: '#d3d3d3'
       }
-
     }
   }
 
   render() {
     return (
-      <div style={this.styles.container}>Welcome, {this.props.userName}</div>
+      <header style={this.styles.navigation}>
+        <nav>
+          <ul>
+            <li><Link to='/'>Real-time</Link></li>
+            <li><Link to='/stats'>Stats</Link></li>
+            <li><Link to='/analytics'>Analytics</Link></li>
+          </ul>
+        </nav>
+        <div style={this.styles.container}>Welcome, {this.props.userName}</div>
+      </header>
     )
   }
 
