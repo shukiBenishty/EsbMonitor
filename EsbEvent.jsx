0af7fc4289f4c2b69c43c74a455a76c243f5a713
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-15 02:12:00 +0200

    Update EsbEvent.jsx

--- EsbEvent.jsx
+++ EsbEvent.jsx
@@ -17,6 +17,13 @@ class EsbEvent extends React.Component<Props> {
     super(props);
 
     this.styles = {
+      esbBox: {
+        backgroundColor: '#fff',
+        border: '1px',
+        borderColor: 'rgb(235, 235, 235)',
+        borderStyle: 'solid',
+        justifyContent: 'flex-start'
+      },
       container: {
         display: "flex",
         flexDirection: "row",
@@ -52,18 +59,32 @@ class EsbEvent extends React.Component<Props> {
 
     let _combinedStyle = [...this.styles.esbFirstField, {marginLeft: "10px"}];
 
-    var statusClass = classNames({
+    var statusClass = classNames('divider-dash', {
       'infoText': this.props.data.status == "INFO",
       'warningText': this.props.data.status == "WARNING",
       'errorText': this.props.data.status == "ERROR"
     });
 
-    return (<div style={this.styles.container}>
-              <div style={this.styles.esbFirstField}>{this.props.data.storyId}</div>
-              <div style={this.styles.esbField} className={statusClass}>{this.props.data.status}</div>
-              <div style={this.styles.esbLongField}>{issued}</div>
-              <div style={this.styles.esbField}>{this.props.data.eventId}</div>
-            </div>);
+    return (<div style={this.styles.esbBox} className="media align-items-center">
+      <a className="flexbox align-items-center flex-grow gap-items text-truncate">
+        <div className="media-body text-truncate">
+          <h6>{this.props.data.serviceName}</h6>
+          <small>
+            <span>{this.props.data.storyId}</span>
+            <span className="divider-dash">{issued}</span>
+            <span className={statusClass}>{this.props.data.status}</span>
+          </small>
+        </div>
+          <span class="lead text-fade">{this.props.data.message}</span>
+      </a>
+    </div>);
+
+    // return (<div style={this.styles.container}>
+    //           <div style={this.styles.esbFirstField}>{this.props.data.storyId}</div>
+    //           <div style={this.styles.esbField} className={statusClass}>{this.props.data.status}</div>
+    //           <div style={this.styles.esbLongField}>{issued}</div>
+    //           <div style={this.styles.esbField}>{this.props.data.eventId}</div>
+    //         </div>);
   }
 
 };
