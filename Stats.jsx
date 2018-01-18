cc4e964696c3faccb47e819c91823c1dbdb2f7ce
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-18 20:24:19 +0200

    Update Stats.jsx

--- Stats.jsx
+++ Stats.jsx
@@ -25,16 +25,22 @@ class Stats extends React.Component<Props, State> {
       fromDate: null,
       tillDate: null,
       selectedServices: '',
-      renderChart: false
+      renderChart: false,
+      categories: [],
+      selectedCategory: ''
     }
 
     this.styles = {
       selectorStyle : {
         width: "400px"
+      },
+      categoriesSelectorStyle: {
+        width: "200px"
       }
     }
 
     this._apply = this._apply.bind(this);
+    this._updateCategory = this._updateCategory.bind(this);
     this._fromDateChanged = this._fromDateChanged.bind(this);
     this._tillDateChanged = this._tillDateChanged.bind(this);
     this._serviceNameChanged = this._serviceNameChanged.bind(this);
@@ -47,6 +53,12 @@ class Stats extends React.Component<Props, State> {
     this.setState({
       renderChart: true
     })
+  }
+
+  _updateCategory(newCategory: string) {
+    this.setState({
+      selectedCategory: newCategory
+    });
   }
 
   _fromDateChanged(_date) {
@@ -67,17 +79,35 @@ class Stats extends React.Component<Props, State> {
 
     this.setState({ selectedServices });
     console.log(`Selected: ${selectedServices}`);
-
-    // this.setState({
-    //   services: [...this.state.services, item]
-    // });
-
   }
 
   // shouldComponentUpdate() {
   //   // prevent rendering after dates changes
   //   return false;
   // }
+
+  async componentDidMount() {
+    // const res = await fetch('http://esb01node01/ESBUddiApplication/api/Categories');
+    // let _categories = await res.json();
+    // let _cats = _categories.map( (category, index) => {
+    //   return {
+    //       value: category.CategoryId,
+    //       label: category.CategoryName
+    //     }
+    // });
+    // this.setState({
+    //   categories: _cats
+    // });
+
+    // Mock
+    let promise = EsbAPI.getAllCategories();
+    promise.then( _categories => {
+      this.setState({
+        categories: _categories
+      })
+    })
+
+  };
 
   render() {
 
@@ -113,6 +143,14 @@ class Stats extends React.Component<Props, State> {
                   <div className="media-list media-list-divided media-list-hover">
                     <header className="flexbox align-items-center media-list-header bg-transparent b-0 py-16 pl-20">
                       <div className="flexbox align-items-center">
+                      <Select style={this.styles.categoriesSelectorStyle}
+                        name="categoriesSelector"
+                        ref={(ref) => { this.select = ref; }}
+                        simpleValue
+                        options={this.state.categories}
+                        value={this.state.selectedCategory}
+                        onChange={this._updateCategory}
+                      />
                       <Select style={this.styles.selectorStyle}
                               multi
                               simpleValue
@@ -144,5 +182,60 @@ class Stats extends React.Component<Props, State> {
   }
 
 };
+
+// Mocks
+
+// [
+//   {"CategoryId":30,"CategoryName":"Active Directory"},
+//   {"CategoryId":0,"CategoryName":"ALL"},
+//   {"CategoryId":28,"CategoryName":"CRM"},
+//   {"CategoryId":33,"CategoryName":"SAP"},
+//   {"CategoryId":32,"CategoryName":"UC4"},
+//   {"CategoryId":20,"CategoryName":"ארכיון"},
+//   {"CategoryId":23,"CategoryName":"ארנונה"},
+//   {"CategoryId":3,"CategoryName":"דיגיתל"},
+//   {"CategoryId":25,"CategoryName":"זימון תורים"},
+//   {"CategoryId":16,"CategoryName":"חינוך"},
+//   {"CategoryId":51,"CategoryName":"חניונים"},
+//   {"CategoryId":10,"CategoryName":"טלאול"},
+//   {"CategoryId":41,"CategoryName":"מאיה"},
+//   {"CategoryId":27,"CategoryName":"מובייל"},
+//   {"CategoryId":4,"CategoryName":"מחו\"ג"},
+//   {"CategoryId":13,"CategoryName":"מסופונים"},
+//   {"CategoryId":43,"CategoryName":"מערך פלילי"},
+//   {"CategoryId":37,"CategoryName":"מערכת לוגיסטית"},
+//   {"CategoryId":34,"CategoryName":"משאבי אנוש"},
+//   {"CategoryId":1,"CategoryName":"משרד התחבורה"},
+//   {"CategoryId":19,"CategoryName":"משרדים ממשלתיים"},
+//   {"CategoryId":21,"CategoryName":"נכסים"},
+//   {"CategoryId":5,"CategoryName":"עירייה זמינה"},
+//   {"CategoryId":6,"CategoryName":"עמ\"ל"},
+//   {"CategoryId":42,"CategoryName":"רישוי בניה תושב"},
+//   {"CategoryId":15,"CategoryName":"רישוי מקוון"},
+//   {"CategoryId":48,"CategoryName":"רישוי עסקים מקוון"},
+//   {"CategoryId":2,"CategoryName":"שירותי מיקום"},
+//   {"CategoryId":24,"CategoryName":"שירותים חיצוניים"},
+//   {"CategoryId":44,"CategoryName":"תיאום הנדסי"},
+//   {"CategoryId":12,"CategoryName":"תשתיות אינטגרציה"}
+// ]
+
+class EsbAPI {
+  static getAllCategories() {
+    return new Promise( (resolve, reject) => {
+      setTimeout( () => {
+        resolve(_.assign([], [
+            {value: 'משרד התחבורה', label: 'משרד התחבורה'},
+            {value: 'שירותי מיקום', label: 'שירותי מיקום'},
+            {value: 'דיגיתל', label: 'דיגיתל'},
+            {value: 'מחו"ג', label: 'מחו"ג'},
+            {value: 'עירייה זמינה', label: 'עירייה זמינה'},
+            {value: 'ארנונה', label: 'ארנונה'},
+            {value: 'CRM', label: 'CRM'},
+        ]
+      ))
+      }, 1000);
+    })
+  }
+}
 
 export default Stats;
