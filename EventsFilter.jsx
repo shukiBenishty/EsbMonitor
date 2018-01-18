39edacf34129c1d0cfd74444943d5f2277e63e9f
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-18 03:04:58 +0200

    Add files via upload

--- EventsFilter.jsx
+++ EventsFilter.jsx
@@ -1,20 +1,36 @@
+// @flow
 import React from 'react';
 import { connect } from 'react-redux'
 
-class EventsFilter extends React.Component {
+type Props = {
+
+};
+
+type State = {
+  filterValue: String,
+  socketSwitch: Boolean
+}
+
+class EventsFilter extends React.Component<Props, State> {
 
   constructor(props) {
     super(props);
 
     this.styles = {
       filterStyle: {
-        backgroundColor: '#d3d3d3'
+        backgroundColor: '#d3d3d3',
+        display: 'flex',
+        flexDirection: 'row'
       }
     }
 
     this.state ={
-      filterValue: ''
+      filterValue: '',
+      socketSwitch: true
     }
+
+    this._toggleSocket = this._toggleSocket.bind(this);
+    this._cleanEvents = this._cleanEvents.bind(this);
   }
 
   updateFilterValue(evt) {
@@ -31,9 +47,37 @@ class EventsFilter extends React.Component {
     })
   }
 
+  _toggleSocket() {
+
+    this.setState({
+      socketSwitch: !this.state.socketSwitch
+    }, () => { // supply callback because setState is async,
+               // so change to socketSwitch is not applied immediately
+      this.props.dispatch({
+        type: 'TOOGLE_SOCKET',
+        switch: this.state.socketSwitch
+      });
+    })
+
+  }
+
+  _cleanEvents() {
+    this.props.dispatch({
+      type: 'CLEAN_EVENTS'
+    })
+  }
+
   render() {
     return(
       <div style={this.styles.filterStyle}>
+        <button className="btn btn-danger btn-round btn-square"
+          onClick={this._toggleSocket}>
+          <i className="ti-power-off"></i>
+        </button>
+        <button className="btn btn-info btn-round btn-square"
+          onClick={this._cleanEvents}>
+          <i className="ti-eraser"></i>
+        </button>
         <input type="text"
                value={this.state.filterValue}
                placeholder="Filter"
