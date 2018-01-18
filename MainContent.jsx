357349770b8bded63b86dc37581b1676bd3f0c41
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-18 03:05:30 +0200

    Update MainContent.jsx

--- MainContent.jsx
+++ MainContent.jsx
@@ -2,17 +2,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Dashboard from './Dashboard';
 import EventList from './EventList';
 import Stats from './Stats';
 import Analyze from './Analyze';
+import EsbAdmin from './EsbAdmin';
+import EsbTest from './EsbTest';
 
 class MainContent extends React.Component<{}> {
 
   render() {
     return (<Switch>
-                  <Route exact path='/' component={EventList} />
-                  <Route path='/stats' component={Stats} />
+                  <Route exact path='/' component={Dashboard} />
+                  <Route path='/realtime' component={EventList} />
+                  <Route path='/stat' component={Stats} />
                   <Route path='/analytics' component={Analyze} />
+                  <Route path="/admin" component={EsbAdmin} />
+                  <Route path="/test" component={EsbTest} />
             </Switch>);
   }
 
