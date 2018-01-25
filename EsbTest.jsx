ccc589bc0b29de4cd84e4e13e3c306f2e46bb466
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-25 19:32:05 +0200

    Update EsbTest.jsx

--- EsbTest.jsx
+++ EsbTest.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
-import { Chart } from 'react-google-charts';
+import { QueryRenderer, graphql } from 'react-relay';
+
+import environemnt from './Environment';
+import EsbTestItem from './EsbTestItem';
+
+const sericesQuery = graphql`
+  query EsbTest_Serices_Query {
+    services {
+        ...EsbTestItem_service
+    }
+  }
+`;
 
 class EsbTest extends React.Component {
 
+  renderServices({error, props}) {
+    if( error ) {
+      return <div>{error.message}</div>
+    } else if( props ) {
+      return (<div>{
+                props.services.map( (service, index) => {
+                  return <EsbTestItem key={index} service={service} />
+                })
+              }
+              </div>);
+    }
+
+    return <div>Loading...</div>
+  }
+
   render() {
 
-    let chartColumns = [
-      {"id":"Stage","type":"string"},
-      {"id":"Start","type":"date"},
-      {"id":"End","type":"date"}
-    ];
-
-    let chartRows = [
-      ["Pre-processing",new Date("2018-01-17T22:00:01.000Z"), new Date("2018-01-17T22:00:01.050Z")],
-      ["Cross-domain",  new Date("2018-01-17T22:00:01.050Z"), new Date("2018-01-17T22:00:03.000Z")],
-      ["Post-processing", new Date("2018-01-17T22:00:03.000Z"), new Date("2018-01-17T22:00:03.640Z")]
-    ];
-
-    return (<main className="main-container maxHeight">
-              <Chart chartType="Timeline"
-                 columns={chartColumns}
-                 rows={chartRows}
-                 options='width:600px'
-                 width='100%'
-                 chartPackage='timeline'
-                 />
-            </main>);
+    return (<div>
+              <QueryRenderer
+                  environment={environemnt}
+                  query={sericesQuery}
+                  variables={{}}
+                  render={this.renderServices}
+                />
+            </div>);
   }
 
 };
