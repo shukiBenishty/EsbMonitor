21efdfae35712f7d34bb3b7d05420d53e4184b88
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-27 14:20:29 +0200

    Update client.jsx

--- client.jsx
+++ client.jsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter }  from 'react-router-dom';
+import { installRelayDevTools } from 'relay-devtools';
 
 import openSocket from 'socket.io-client';
 
 import reducers from './reducers';
 import App from './App.jsx';
 
+// Useful for debugging, but remember to remove for a production deploy.
+installRelayDevTools();
+
 let store = createStore(reducers,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const socket = openSocket('http://10.0.0.1:8000');
+const socket = openSocket('http://localhost:8000');
 
 socket.on('esbEvent', data => {
 
