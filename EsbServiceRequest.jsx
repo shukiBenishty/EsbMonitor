08c92878310cd6f8e775b0c02a2dbda5c6daeec1
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-07 12:57:26 +0200

    Update EsbServiceRequest.jsx

--- EsbServiceRequest.jsx
+++ EsbServiceRequest.jsx
@@ -2,20 +2,49 @@ import React from 'react';
 
 class EsbServiceRequest extends React.Component {
 
+  constructor(props) {
+
+    super(props);
+
+    this.styles = {
+      serviceMenu: {
+        position: "absolute",
+        top: "19px",
+        left: "-147px",
+        willChange: "top, left"
+      }
+
+    }
+
+  }
+
   render() {
 
     let serviceRequest = this.props.serviceRequest;
 
-    return <div className="media align-items-center">
-              <a className="flexbox align-items-center flex-grow gap-items">
+    return <div className="media align-items-center flexbox">
+              <a className="align-items-center flex-grow gap-items">
                 <div className="media-body text-truncate">
                   <h6>{serviceRequest.address}</h6>
                   <small>
-                    <span>{serviceRequest.objectId}</span>
-                    <span className="divider-dash">SLA: {serviceRequest.domain}</span>
+                    <span>{serviceRequest.operationName}</span>
+                    <span className="divider-dash">Domain: {serviceRequest.domain}</span>
                   </small>
                 </div>
               </a>
+              <div className="dropdown">
+                <a className="text-lighter" data-toggle="dropdown">
+                  <i className="ti-more-alt rotate-90"></i>
+                </a>
+                <div className="dropdown-menu dropdown-menu-right"
+                    x-placement="bottom-end"
+                    style={this.styles.serviceMenu}>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item">
+                      <span className="icon ti-cloud-up">Publish</span>
+                    </a>
+                </div>
+              </div>
           </div>
   }
 
