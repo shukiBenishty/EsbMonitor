4850260958b39229e46a77ebce84b99cdcdede5b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-12 03:12:18 +0200

    Update Navigation.jsx

--- Navigation.jsx
+++ Navigation.jsx
@@ -1,5 +1,48 @@
+// @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const links = [{
+  id: 1,
+  type: 'menu-item',
+  to: '/',
+  title: 'Dashboard',
+  icon: 'icon ti-dashboard',
+  badge: null
+}, {
+  id: 2,
+  type: 'menu-item',
+  to: '/realtime',
+  title: 'Realtime',
+  icon: 'icon ti-layout-list-thumb',
+  badge: { count: 3, type: 'badge-danger'}
+}, {
+  id: 3,
+  type: 'menu-item',
+  to: '/stat',
+  title: 'Stats',
+  icon: 'icon ti-pulse',
+  badge: null
+}, {
+  id: 4,
+  type: 'menu-item',
+  to: '/analytics',
+  title: 'Analytics',
+  icon: 'icon ti-layers-alt',
+  badge: null
+}, {
+  id: 5,
+  type: 'menu-divider'
+}, {
+  id: 6,
+  type: 'menu-item',
+  to: '/admin',
+  title: 'Admin',
+  icon: 'icon ti-settings',
+  badge: null
+}
+]
 
 class Navigation extends React.Component {
 
@@ -12,6 +55,18 @@ class Navigation extends React.Component {
         height: "48px"
       }
     }
+
+    this.state = {
+      currentLink: 1
+    }
+
+    this.linkClicked = this.linkClicked.bind(this);
+  }
+
+  linkClicked(linkNumber: number) {
+    this.setState({
+      currentLink: linkNumber
+    })
   }
 
   render() {
@@ -23,48 +78,34 @@ class Navigation extends React.Component {
                 </header>
                 <nav className="sidebar-navigation ps-container">
                   <ul className="menu menu-sm">
-                     <li className="menu-item active">
-                        <Link className="menu-link" to="/">
-                          <span className="icon ti-dashboard">
-                          </span>
-                          <span className="title">Dashboard
-                          </span>
-                        </Link>
-                    </li>
-                    <li className="menu-item">
-                      <Link className="menu-link" to='/realtime'>
-                        <span className="icon ti-layout-list-thumb"></span>
-                        <span className="title">Realtime</span>
-                      </Link>
-                    </li>
-                    <li className="menu-item">
-                      <Link className="menu-link" to='/stat'>
-                        <span className="icon ti-pulse"></span>
-                        <span className="title">Stats</span>
-                      </Link>
-                    </li>
-                    <li className="menu-item">
-                      <Link className="menu-link" to='/analytics'>
-                        <span className="icon ti-layers-alt"></span>
-                        <span className="title">Analytics</span>
-                        <span className="badge badge-pill badge-info">2</span>
-                      </Link>
-                    </li>
-                    <li className="menu-divider">
-                    </li>
-                    <li>
-                      <Link className='menu-link' to='/admin'>
-                        <span className="icon ti-settings"></span>
-                        <span className="title">Admin</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className='menu-link' to='/test'>
-                        <span className="icon ti-help"></span>
-                        <span className="title">Test</span>
-                      </Link>
-                    </li>
+                  {
+                    links.map( (link, index) => {
+
+                        let linkClassName = classNames('menu-item', {
+                          'active': link.id == this.state.currentLink
+                        })
+
+                        if( link.type == 'menu-divider' ) {
+                            linkClassName = link.type;
+                            return (<li key={index+1} className={linkClassName} />)
+                        } else {
+
+                          //if( link.badge )
+                          //<span className="badge badge-pill badge-danger">3</span>
+
+                          return (<li className={linkClassName}>
+                                    <Link key={index} className="menu-link"
+                                      onClick={ () => this.linkClicked(index+1) }
+                                      to={link.to}>
+                                        <span className={link.icon}></span>
+                                        <span className="title">{link.title}</span>
+                                    </Link>
+                                 </li>)
+                        }
+                    })
+                  }
                   </ul>
+
                 </nav>
             </aside>);
   }
