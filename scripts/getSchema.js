3d5d57e2df3946f11d5459b414a68579947b8e85
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-14 01:38:03 +0200

    Create getSchema.js

---
+++ scripts/getSchema.js
@@ -0,0 +1,24 @@
+var fetch = require('node-fetch');
+var fs = require('fs');
+const {
+  buildClientSchema,
+  introspectionQuery,
+  printSchema,
+} = require('graphql/utilities');
+
+console.log(introspectionQuery);
+
+fetch('http://localhost:3001/graphql', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ 'query': introspectionQuery }),
+})
+.then(res => res.json())
+  .then(res => {
+    console.log(res);
+    const schemaString = printSchema(buildClientSchema(res.data));
+    fs.writeFileSync('./schemas/schema2.graphql', schemaString.replace("\"\"\"", ""));
+  });
