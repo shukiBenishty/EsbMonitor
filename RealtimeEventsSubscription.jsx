24ee8bddb83bbccc783db47077e6447b673418d0
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-17 23:27:11 +0200

    Add files via upload

---
+++ RealtimeEventsSubscription.jsx
@@ -0,0 +1,39 @@
+// @flow
+import { graphql, requestSubscription } from 'react-relay';
+import environment from './Environment';
+
+const realtimeEventsSubscription = graphql`
+  subscription RealtimeEventsSubscription_Subscription {
+    traceAdded {
+      id
+    }
+  }
+`;
+
+export default (serviceId) => {
+
+
+    const subscriptionConfig = {
+      subscription: realtimeEventsSubscription,
+      variables: {},
+      updater: proxyStore => {
+        const createTraceField = proxyStore.getRootField('traceAdded');
+        const newTrace = createTraceField.getLinkedRecord('node');
+        // const updatedLink = newVote.getLinkedRecord('link')
+        // const linkId = updatedLink.getValue('id')
+        // const newVotes = updatedLink.getLinkedRecord('_votesMeta')
+        // const newVoteCount = newVotes.getValue('count')
+        //
+        // const link = proxyStore.get(linkId)
+        // link.getLinkedRecord('votes').setValue(newVoteCount, 'count')
+      },
+      onError: error => {
+        console.log(`An error occured:`, error);
+      }
+    };
+
+    requestSubscription(
+      environment,
+      subscriptionConfig
+    )
+}
