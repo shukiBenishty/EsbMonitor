6e1c94abaa7131b212a02f53fe12b408143f6c0a
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-17 16:06:21 +0200

    Add files via upload

---
+++ SummaryErrors.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { createFragmentContainer, graphql} from 'react-relay';
+import { css } from 'glamor';
+
+const SummaryErrors = ({title}) => {
+
+  let progressBarWidth = "14%";
+
+  let progressBarCss = css({
+      width: progressBarWidth,
+      height: "4px",
+      backgroundColor: "#f96868 !important;"
+  });
+
+  return (<div className="col-lg-4">
+              <div className="card card-body esbCard">
+                <h6>
+                  <span className="text-uppercase esbCaption">{title}</span>
+                  <span className="float-right">
+                    <a className="btn btn-xs btn-primary" href="#">View</a>
+                  </span>
+                </h6>
+                <br />
+                <p className="fs-28 fw-100">3</p>
+                <div className="progress">
+                  <div className="progress-bar bg-danger" role="progressbar" className={progressBarCss}>
+                  </div>
+                </div>
+                <div className="text-gray fs-12">
+                  <i className="ti-stats-down text-danger mr-1"></i>%3 decrease from last hour
+                </div>
+              </div>
+            </div>);
+
+};
+
+export default createFragmentContainer(SummaryErrors,
+graphql`
+fragment SummaryErrors_totals on Runtime {
+  errors(before: 1) {
+    date
+    value
+    id
+  }
+}
+`);
---
+++ SummaryLatency.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { createFragmentContainer, graphql} from 'react-relay';
+import { css } from 'glamor';
+
+const SummaryLatency = ({title}) => {
+
+  let progressBarWidth = "3%";
+
+  let progressBarCss = css({
+      width: progressBarWidth,
+      height: "4px",
+      backgroundColor: "#f96868 !important;"
+  });
+
+  return (<div className="col-lg-4">
+              <div className="card card-body esbCard">
+                <h6>
+                  <span className="text-uppercase esbCaption">{title}</span>
+                  <span className="float-right">
+                    <a className="btn btn-xs btn-primary" href="#">View</a>
+                  </span>
+                </h6>
+                <br />
+                <p className="fs-28 fw-100">0.13 sec.</p>
+                <div className="progress">
+                  <div className="progress-bar bg-danger" role="progressbar" className={progressBarCss}>
+                  </div>
+                </div>
+                <div className="text-gray fs-12">
+                  <i className="ti-stats-down text-danger mr-1"></i>%18 decrease from last hour
+                </div>
+              </div>
+            </div>);
+
+}
+
+export default createFragmentContainer(SummaryLatency,
+graphql`
+  fragment SummaryLatency_totals on Runtime {
+    latency(before:1) {
+      date
+      value
+      id
+    }
+  }
+`);
