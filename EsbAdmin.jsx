270cc31e8d206542fe88f06a3bea3ba374fa4ab4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-21 01:53:02 +0200

    Update EsbAdmin.jsx

--- EsbAdmin.jsx
+++ EsbAdmin.jsx
@@ -1,423 +1,239 @@
 // @flow
 import React from 'react';
-import { requestSubscription, createRefetchContainer, commitMutation, graphql } from 'react-relay';
-import { AutoSizer, List } from 'react-virtualized';
-import Select from 'react-select';
-import { ToastContainer, toast } from 'react-toastify';
-import { css } from 'glamor';
-import classNames from 'classnames';
-import TextField from 'material-ui/TextField';
+import { graphql, requestSubscription } from 'react-relay';
+import { connect } from 'react-redux'
+import * as JsSearch from 'js-search';
 
-import EsbService from './EsbService';
-import EsbServiceRequest from './EsbServiceRequest';
-import AdminServicePanel from './AdminServicePanel';
+import EventsFilter from './EventsFilter';
+import EsbEvent from './EsbEvent';
+import EsbStatus from './EsbStatus';
 import environment from './Environment';
 
- const addServiceMutation = graphql`
-   mutation EsbAdminMutation(
-     $input: ServiceInput
-   ) {
-     addService(input: $input) {
-       id
-       objectId
-       name
-       categoryId
-       environment
-       address
-       sla
-       created
-     }
-   }
- `;
+//import RealtimeEventsSubscription from './RealtimeEventsSubscription';
 
- const deleteServiceRequestSubscription = graphql`
-   subscription EsbAdmin_ServiceRequestDeleted_Subscription {
-     serviceRequestDeleted {
-       id
-     }
-   }
- `;
+import { AutoSizer, List , Table, Column } from 'react-virtualized';
+import 'react-virtualized/styles.css'; // no CSS modules!!!
+
+const realtimeEventsSubscription = graphql`
+  subscription EventList_Subscription {
+    traceAdded {
+      id
+      storyId
+      time
+      serviceName
+      serviceId
+      message
+      eventId
+      status
+    }
+  }
+`;
 
 type Props = {
+  eventId: number,
+  issued: Date
 };
 
 type State = {
-  servicePanelVisible: boolean,
-  selectedCategory: Object,
-  currentServicesPage: number
+    esbEvents: Array<Object>
 }
 
-class EsbAdmin extends React.Component<Props, State> {
+class EventList extends React.Component<Props, State> {
 
-  // state = {
-  //   servicePanelVisible: false
-  // };
+  vTable: Object;
+  styles: Object;
+  search: Object;
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      servicePanelVisible: false,
-      selectedCategory: {},
-      categories: [],
-      currentServicesPage: 1,
-    };
-
-    this.styles = {
-      categoriesSelectorStyle: {
-        position: 'absolute',
-        right: '0px',
-        marginTop: '-30px',
-        marginRight: '30px',
-        width: '14%',
-        zIndex: '100'
-      },
-      listTitle: {
-        borderBottom: "0"
-      },
+      esbEvents: [],
     }
 
-    this._openServicePanel = this._openServicePanel.bind(this);
-    this._closeServicePanel = this._closeServicePanel.bind(this);
-    this._updateCategory = this._updateCategory.bind(this);
+    this.styles = {
+      eventListFrame: {
+        height: "92%",
+        minHeigth: "410px",
+        display: "flex",
+        flexDirection: "column"
+      }
 
-    this.pageClicked = this.pageClicked.bind(this);
+    }
 
     this.rowRenderer = this.rowRenderer.bind(this);
-    this.rowRequestRenderer = this.rowRequestRenderer.bind(this);
+    this.publishEsbEvent = this.publishEsbEvent.bind(this);
+  }
 
-    this._serviceAdding = this._serviceAdding.bind(this);
-    this._serviceAdded = this._serviceAdded.bind(this);
+  publishEsbEvent(payload) {
+
+    this.props.dispatch({
+      type: 'NEW_EVENT',
+          data: {
+            storyId: payload.storyId,
+            serviceName: payload.serviceName,
+            message: payload.message,
+            eventId: payload.eventId,
+            issued: payload.time,
+            status: payload.status
+          }
+    })
   }
 
   componentDidMount() {
+    this.search = new JsSearch.Search('eventId');
+    // this.search.addDocuments([{eventId: 'ab'}, {eventId: 'cd'}, {eventId: 'ef'}]);
+    //this.search.addIndex('eventId');
 
+    const self = this;
     const subscriptionConfig = {
-      subscription: deleteServiceRequestSubscription,
+      subscription: realtimeEventsSubscription,
       variables: {},
-      updater: (proxyStore: RecordSourceSelectorProxy) => {
+      onNext: payload => {
 
-        const payloadProxy = proxyStore.getRootField('serviceRequestDeleted');
-        const _id = payloadProxy.getValue('id');
+        // TBD: Temporay solution: change this to use Relay fragment!
+        self.publishEsbEvent(payload.traceAdded);
 
-        proxyStore.delete(_id);
+      },
+      updater: proxyStore => {
+
+        //  Reading values off the Payload
+        const rootField = proxyStore.getRootField('traceAdded');
+        const __type = rootField.getType();
+        const __status = rootField.getValue('status');
+
+        // Reading Values off the Relay Store
+        let root = proxyStore.getRoot();
+        let _type = root.getType();
+        let runtimeRecord = root.getLinkedRecord('runtime');
+        if( runtimeRecord ) {
+          let totalCallsRecords = runtimeRecord.getLinkedRecords('totalCalls', {before: 1});
+          if( totalCallsRecords ) {
+            let totalCalls = totalCallsRecords[0].getValue('value');
+            totalCallsRecords[0].setValue( totalCalls + 1, "value");
+          }
+
+          if( __status == 'ERROR' ) {
+            let errorsRecors = runtimeRecord.getLinkedRecords('errors', {before: 1});
+            if( errorsRecors ) {
+              let totalErrors = errorsRecors[0].getValue('value');
+              errorsRecors[0].setValue( totalErrors + 1, 'value');
+            }
+          }
+        }
+      },
+      onError: error => {
+        console.log(`An error occured:`, error);
       }
     };
 
-    let subscription = requestSubscription(
+    this.subscription = requestSubscription(
       environment,
       subscriptionConfig
-    );
-  }
-
-  _serviceAdding() {
-      this.toastId = toast("Adding Service to Repository", { autoClose: false});
-  }
-
-  _serviceAdded({isAdded, message}) {
-
-    if( isAdded ) {
-      toast.update(this.toastId,
-                    {
-                      render: message,
-                      type: toast.TYPE.SUCCESS,
-                      autoClose: 3000,
-                      className: css({
-                        transform: "rotateY(360deg)",
-                        transition: "transform 0.6sec"
-                      })
-                    });
-
-    } else {
-      toast.update(this.toastId,
-                    {
-                      render: message,
-                      type: toast.TYPE.ERROR,
-                      autoClose: 5000,
-                    }
-                  );
-    }
-
-  }
-
-  _openServicePanel() {
-
-    this.setState({
-      servicePanelVisible: true
-    })
-  }
-
-   _closeServicePanel() {
-
-    this.setState({
-        servicePanelVisible: false
-    })
-  }
-
-  _updateCategory(newCategory) {
-
-    let disableCategoriesSelector = !newCategory ? true : false;
-
-    this.setState({
-      selectedCategory: newCategory,
-      serviceSelectorDisabled: disableCategoriesSelector,
-      selectedServices: ''
-    });
-
-    this.props.relay.refetch(
-      (prev) => (
-        {
-          categoryId: newCategory ? newCategory.value : null,
-          page: this.state.currentServicesPage,
-          pageSize: 10
-        }
-      ),
-      null,
-      null,
-      { force: false } // Network layer for this app is configured to use cache (vis QueryResponseCache)
-                       // This parameter has the meaning for it.
-                       // Although it is redundant here because the default is false.
     )
   }
 
-  pageClicked(pageNumber: number) {
-
-    this.props.relay.refetch(
-      (prev) => (
-        {
-          categoryId: this.state.selectedCategory ? this.state.selectedCategory.value : null,
-          page: pageNumber,
-          pageSize: 10
-        }
-      ),
-      null,
-      null,
-      { force: false } // Network layer for this app is configured to use cache (vis QueryResponseCache)
-                       // This parameter has the meaning for it.
-                       // Although it is redundant here because the default is false.
-    );
-
-    this.setState({
-      currentServicesPage: pageNumber
-    })
-
+  componentWillUnmount() {
+    //this.subscription.dispose();
   }
 
-  rowRenderer({
-    key,
-    index,
-    isScrolling,
-    isVisible,
-    style
-  }) {
+  componentWillReceiveProps(nextProps){
 
-    //this.vServicesList.scrollToRow(index);
+    if( nextProps.filterValue ) {
+      let filteredEvents = this.search.search(nextProps.filterValue);
+      console.log(filteredEvents.length);
+    }
 
-    let _services = this.props.repository.services.list;
+    const esbEvent = {
+      storyId: nextProps.storyId,
+      serviceName: nextProps.serviceName,
+      message: nextProps.message,
+      eventId: nextProps.eventId,
+      issued: nextProps.issued,
+      status: nextProps.status
+    }
 
-    return <EsbService key={index}
-                       service={_services[index]} />
+    this.setState( prevState => ({
+      esbEvents: [...this.state.esbEvents, {
+                      storyId: esbEvent.storyId,
+                      serviceName: esbEvent.serviceName,
+                      message: esbEvent.message,
+                      eventId: esbEvent.eventId,
+                      issued: esbEvent.issued,
+                      status: esbEvent.status
+                    }
+                ]
+    }));
+
+    this.search.addDocuments({
+                    storyId: esbEvent.storyId,
+                    serviceName: esbEvent.serviceName,
+                    eventId: esbEvent.eventId.toString(),
+                    issued: esbEvent.issued
+                  });
+    this.search.addIndex('eventId');
+    this.search.addIndex('serviceName');
+    this.search.addIndex('message');
+    this.search.addIndex('issued');
   }
 
-  rowRequestRenderer({
-    key,
-    index,
-    isScrolling,
-    isVisible,
-    style
+  rowRenderer ({
+    key,         // Unique key within array of rows
+    index,       // Index of row within collection
+    isScrolling, // The List is currently being scrolled
+    isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+    style        // Style object to be applied to row (to position it)
   }) {
 
-    //this.vServiceRequestList.scrollToRow(index);
+    if( this.state.esbEvents.length > 0 ) {
 
-    let requests = this.props.repository.serviceRequests
-                   .filter( sReq => sReq != null);
+      this.vTable.scrollToRow(index);
 
-    return <EsbServiceRequest key={index}
-                              serviceRequest={requests[index]} />
+      return (<div key={key} style={style}>
+                <EsbEvent data={this.state.esbEvents[index]}/>
+              </div>)
+    } else {
+      return(<div key={key} style={style}>No events</div>)
+    }
 
   }
 
   render() {
 
-    let _categories = this.props.categories.map( (category, index) => {
-        return {
-          value: category.objectId,
-          label: category.name
-        }
-    })
+    return(<main className="main-container maxHeight">
+              <div className="main-content maxHeight">
+                <EventsFilter />
+                <div style={this.styles.eventListFrame}>
+                  <AutoSizer >
+                      {({ height, width }) => (
+                          <List ref={c => { this.vTable = c; }}
+                            width={width}
+                            height={height}
+                            rowHeight={60}
+                            rowCount={this.state.esbEvents.length}
+                            rowRenderer={this.rowRenderer}>
 
-    let _services = this.props.repository.services.list;
-    let totalServices = this.props.repository.services.totalItems;
-
-    var servicePanelClass = classNames('quickview', 'quickview-lg', {
-       'reveal': this.state.servicePanelVisible
-    });
-
-    const { selectedCategory } = this.state;
-    const _value = selectedCategory && selectedCategory.value;
-
-    let _serviceRequests = this.props.repository.serviceRequests
-                           .filter( sReq => sReq != null );
-
-    return (<main className="main-container maxHeight">
-                <ToastContainer />
-                <div className="main-content maxHeight">
-                  <div className="media-list media-list-divided media-list-hover maxHeight">
-                    <header className="media-list-header bg-transparent b-0 py-16 pl-20">
-                      <div style={this.styles.categoriesSelectorStyle}>
-                        <Select
-                            className='categoriesSelector'
-                            name="categoriesSelector"
-                            placeholder="Select category"
-                            options={_categories}
-                            value={_value}
-                            onChange={this._updateCategory}
-                        />
-                      </div>
-                    </header>
-                    <div className="row">
-                      <div className="col-lg-3 tab-content">
-                          <div className="card">
-                            <ul className="nav nav-tabs nav-lg _flexColumn">
-                              <li className="active">
-                                <a data-toggle="tab" className="nav-link active show" href="#tab1">Services</a>
-                              </li>
-                              <li>
-                                <a data-toggle="tab" className="nav-link" href="#tab2">Requests</a>
-                              </li>
-                            </ul>
-                          </div>
-                      </div>
-
-                      <div className="col-lg-9 tab-content maxHeight">
-                        <div className="card tab-pane maxHeight fade in active show" id="tab1">
-                          <h4 style={this.styles.listTitle}
-                              className="card-title fw-400">Published Services</h4>
-
-                          <div className="row">
-                              <div className="col-lg-10"></div>
-                              <div className="col-lg-2">
-                                  <nav>
-                                    <ul className="pagination pagination-circle">
-                                      {
-                                        [1,2,3].map( pageNumber => {
-
-                                          var pageNumberClassName = classNames('page-item', {
-                                            'active': pageNumber == this.state.currentServicesPage
-                                          });
-
-                                          return <li key={pageNumber} className={pageNumberClassName}
-                                                      onClick={()=>this.pageClicked(pageNumber)} >
-                                                    <div className="page-link">{pageNumber}</div>
-                                                 </li>
-
-                                        })
-                                      }
-                                    </ul>
-                                  </nav>
-                              </div>
-                          </div>
-                          <AutoSizer>
-                            {
-                              ({height, width}) => (
-
-                                <List className="esbRepositoryList"
-                                  ref={ c => { this.vServicesList = c; }}
-                                  width={width}
-                                  height={height}
-                                  autoHeight={true}
-                                  rowHeight={60}
-                                  rowGetter={ ({index}) => _services[index] }
-                                  rowCount={_services.length}
-                                  rowRenderer={this.rowRenderer}
-                                />
-
-                              )
-                            }
-                          </AutoSizer>
-
-                        </div>
-                        <div className="card tab-pane fade" id="tab2">
-                          <h4 style={this.styles.listTitle}
-                              className="card-title fw-400">Publish Requests</h4>
-
-                            <AutoSizer>
-                              {
-                                ({height, width}) => (
-
-                                  <List className="esbRepositoryList"
-                                    ref={ c => { this.vServiceRequestList = c; }}
-                                    width={width}
-                                    height={height}
-                                    autoHeight={true}
-                                    rowHeight={60}
-                                    rowGetter={ ({index}) => _serviceRequests[index] }
-                                    rowCount={_serviceRequests.length}
-                                    rowRenderer={this.rowRequestRenderer}
-                                  />
-
-                                )
-                              }
-                            </AutoSizer>
-
-                        </div>
-                      </div>
-
-                    </div>
-                  </div>
+                          </List>
+                        )}
+                  </AutoSizer>
                 </div>
-                <div className="fab fab-fixed">
-                 <a className="btn btn-float btn-primary" onClick={this._openServicePanel}>
-                   <i className="ti-plus"></i>
-                 </a>
-                </div>
-                <div className={servicePanelClass}>
-                  <AdminServicePanel onClose={this._closeServicePanel}
-                                     onServiceAdding={this._serviceAdding}
-                                     onServiceAdded={this._serviceAdded}
-                                     categories={_categories} />
-                </div>
-            </main>
-         );
-
+              <EsbStatus filter={this.props.filterValue}/>
+            </div>
+          </main>)
   }
 
-};
-
-export default createRefetchContainer(
-EsbAdmin,
-graphql`
-  fragment EsbAdmin_repository on Repository
-  @argumentDefinitions(
-    categoryId: { type: Int }
-    page: { type: Int, defaultValue: 1 }
-    pageSize: { type: Int, defaultValue: 10}
-  )
-  {
-    services (categoryId: $categoryId, page: $page, pageSize: $pageSize){
-      totalItems
-      list {
-        ...EsbService_service
-      }
-    }
-    serviceRequests {
-      ...EsbServiceRequest_serviceRequest
-    }
 }
-`,
-graphql`
-  query EsbAdmin_Query
-  (
-    $categoryId: Int
-    $page: Int
-    $pageSize: Int
-  )
-  {
-    repository {
-    	...EsbAdmin_repository @arguments(categoryId: $categoryId,
-                                        page: $page,
-                                        pageSize: $pageSize)
-    }
+
+function mapStateToProps(state) {
+  return {
+      storyId: state.storyId,
+      serviceName: state.serviceName,
+      message: state.message,
+      eventId: state.eventId,
+      issued: state.issued,
+      status: state.status,
+      filterValue: state.filterValue
   }
-`
-);
+}
+
+export default connect(mapStateToProps)(EventList);
