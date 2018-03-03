ab322cb56bc4a116c1dc67918dfe5c073b32fa1e
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-03 02:59:54 +0200

    Update Story.jsx

--- src/Story.jsx
+++ src/Story.jsx
@@ -79,12 +79,12 @@ class Story extends React.Component {
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
         >
-          <h3 className="vertical-timeline-element-title">Message received </h3>
-          <h4 className="vertical-timeline-element-subtitle">by External (INT1) environment</h4>
-          <p>
+          <h3 className="vertical-timeline-element-title"><b>Message received</b></h3>
+          <h4 className="vertical-timeline-element-subtitle"><b>by External (INT1) environment</b></h4>
+          <div>
             From 192.5.34.22
-          </p>
-          <p>User INT1\Prd_ClientPool</p>
+          </div>
+          <div>User INT1\Prd_ClientPool</div>
           <p>
             <button className='btn'
                     onClick={this.openModal}>Payload</button>
@@ -93,46 +93,35 @@ class Story extends React.Component {
                 onRequestClose={this.closeModal}
                 isOpen={this.state.modalIsOpen}
                 contentLabel="Example Modal">
-                <div>I am a modal</div>
+                <div>Payload is display here</div>
              </Modal>
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
+          date="+23 ms."
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
         >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <h3 className="vertical-timeline-element-title">Message analyzed</h3>
+          <h4 className="vertical-timeline-element-subtitle">by External (INT1) environment</h4>
           <p>
             Creative Direction, User Experience, Visual Design, SEO, Online Marketing
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2008 - 2010"
+          date="+31 ms."
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          <h3 className="vertical-timeline-element-title">Message routed</h3>
+          <h4 className="vertical-timeline-element-subtitle">To Internal (DOM) environment</h4>
           <p>
             User Experience, Visual Design
           </p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="April 2013"
