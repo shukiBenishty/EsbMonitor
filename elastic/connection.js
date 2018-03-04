d12f6d71f145c65c05acf3b8e985c349f6ea0d11
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-04 16:41:46 +0200

    Update connection.js

--- elastic/connection.js
+++ elastic/connection.js
@@ -1,7 +1,8 @@
 import elasticsearch from 'elasticsearch';
 
 const client = new elasticsearch.Client( {
-  hosts: ['localhost:9200', '10.1.70.47:9200'],
+  //hosts: ['localhost:9200', '10.1.70.47:9200'],
+  hosts: 'localhost:9200',
   //log: 'trace'
 });
 
