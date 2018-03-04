e5ab095451e41e63f29651cb72fbf3563b04c2e3
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-05 01:41:51 +0200

    Add files via upload

---
+++ src/NavigationRenderer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { QueryRenderer, graphql } from 'react-relay';
+import environment from '../Environment';
+import Navigation from './Navigation';
+
+const errorsQuery = graphql`
+	query NavigationRenderer_Query ($daysBefore: Date)
+ {
+		runtime {
+			...Navigation_totals @arguments(before: $daysBefore)
+		}
+	}
+`;
+
+class NavigationRenderer extends React.Component{
+
+  renderErrors({error, props}) {
+    if( error ) {
+      return <div>{error.message}</div>
+    } else if ( props ) {
+      return <Navigation totals={props.runtime}/>;
+    }
+
+    return <div>Loading...</div>
+  }
+
+  render() {
+
+    let queryVariables = {
+			daysBefore: 1 // get errors just for today
+		}
+
+     return <QueryRenderer
+                  environment={environment}
+                  query={errorsQuery}
+                  variables={queryVariables}
+                  render={this.renderErrors}
+            />
+  }
+
+}
+
+export default NavigationRenderer;
