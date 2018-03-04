3f17deee18f927e417cb39345628a80718f54b4b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-04 18:00:49 +0200

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -2,6 +2,7 @@
 import _ from 'lodash';
 
 const INITIAL_STATE = {
+  errorsCount: 0,
   storyId: '',
   serviceName: '',
   message: '',
@@ -9,37 +10,29 @@ const INITIAL_STATE = {
   issued: Date(),
   status: '',
   filterValue: '',
-  socketSwitch: true,
-  categories: []
+  categories: []  
 };
 
 const reducers = (state = INITIAL_STATE, action) => {
 
   switch( action.type ) {
 
-    case 'TOOGLE_SOCKET':
-      state = _.assign({}, state, {
-                                    socketSwitch: action.switch
-                                  })
-      break;
-
-    case 'CLEAN_EVENTS': {
-
-    }
-    break;
-
     case 'NEW_EVENT': {
 
-      if( state.socketSwitch ) {
+      state = _.assign({}, state, {
+                                    storyId: action.data.storyId,
+                                    serviceName: action.data.serviceName,
+                                    message: action.data.message,
+                                    eventId: action.data.eventId,
+                                    issued: new Date(action.data.issued),
+                                    status: action.data.status
+                                 });
+
+      if( action.data.status.localeCompare("ERROR") == 0 ) {
 
         state = _.assign({}, state, {
-                                      storyId: action.data.storyId,
-                                      serviceName: action.data.serviceName,
-                                      message: action.data.message,
-                                      eventId: action.data.eventId,
-                                      issued: new Date(action.data.issued),
-                                      status: action.data.status
-                                   })
+                              errorsCount: ++state.errorsCount
+                            })
       }
     }
     break;
