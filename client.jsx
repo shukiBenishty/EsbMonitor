61bb8a4ceedc2a5d94eeba192fe29ed87ccc8194
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-25 01:04:00 +0300

    Update client.jsx

--- client.jsx
+++ client.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { HashRouter }  from 'react-router-dom';
+import { HashRouter as Router}  from 'react-router-dom';
 import { installRelayDevTools } from 'relay-devtools';
-
-//import openSocket from 'socket.io-client';
 
 import reducers from './src/reducers';
 import App from './src/App.jsx';
@@ -16,27 +14,10 @@ installRelayDevTools();
 let store = createStore(reducers,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// const socket = openSocket('http://localhost:8000');
-//
-// socket.on('esbEvent', data => {
-//
-//   store.dispatch({
-//     type: 'NEW_EVENT',
-//     data: {
-//       storyId: data.storyId,
-//       serviceName: data.serviceName,
-//       message: data.message,
-//       eventId: data.eventId,
-//       issued: data.time,
-//       status: data.status
-//     }
-//   })
-// });
-// socket.emit('subscribeToEsbEvents', ''); // no filter initially
 
 ReactDOM.render(<Provider store={store}>
-                  <HashRouter>
+                  <Router>
                     <App />
-                  </HashRouter>
+                  </Router>
                 </Provider>  ,
                 document.getElementById('root'));
