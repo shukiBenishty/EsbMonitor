1f59abf3c30f5e6631b34d31ed476ccc21aa74bc
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-25 03:22:05 +0300

    Update Analyze.jsx

--- src/Analyze.jsx
+++ src/Analyze.jsx
@@ -1,9 +1,20 @@
 // @flow
 import React from 'react';
+import Loadable from 'react-loadable';
 import { Switch, Route } from 'react-router-dom';
 
 import Story from './Story';
 import Search from './Search';
+
+// const LoadableStoryComponent = Loadable({
+//   loader: () => import('./Story'),
+//   loading: () => { return <div>Loading...</div> }
+// })
+//
+// const LoadableSearchComponent = Loadable({
+//   loader: () => import('./Story'),
+//   loading: () => { return <div>Loading...</div> }
+// })
 
 const Analyze = ({ match }) => {
 
@@ -13,7 +24,7 @@ const Analyze = ({ match }) => {
           <div className='tab-content'>
 
             <Switch>
-              <Route exact path={match.url} component={Search} />
+              <Route path={match.url + '/:searchText'} component={Search} />
               <Route path={match.url + '/story/:storyId'} component={Story} />
             </Switch>
 
