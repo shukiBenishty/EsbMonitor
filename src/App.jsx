de24213bc5a68bd2f08e842602d8fde79a268ab9
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-06-23 03:42:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ const realtimeEventsSubscription = graphql`
     traceAdded {
       id
       storyId
-      time
+      received
+      serviceName
       serviceId
+      message
       status
     }
   }
@@ -30,8 +32,7 @@ class AppLayout extends React.Component {
         storyId: payload.storyId,
         serviceName: payload.serviceName,
         message: payload.message,
-        eventId: payload.eventId,
-        issued: payload.time,
+        issued: payload.received,
         status: payload.status
       }
     })
@@ -65,11 +66,9 @@ class AppLayout extends React.Component {
         let runtimeRecord = root.getLinkedRecord('runtime');
         if( runtimeRecord ) {
 
-          let daysBefore = 7;
-
           let distributionRecord =
             runtimeRecord.getOrCreateLinkedRecord('distribution', 'Series',
-                                                  {daysBefore: daysBefore, servicesIds: [1,3]});
+                                                  {daysBefore: 0, servicesIds: [1,3]});
 
           if( distributionRecord ) {
               let seriesRecords = distributionRecord.getLinkedRecords('series');
@@ -93,20 +92,19 @@ class AppLayout extends React.Component {
               }
           }
 
-          let totalCallsRecords = runtimeRecord.getLinkedRecords('totalCalls', {before: 1});
+          let totalCallsRecords = runtimeRecord.getLinkedRecords('totalCalls', {before: 0});
           if( totalCallsRecords && totalCallsRecords.length > 0
               && totalCallsRecords[0] ) {
             let totalCalls = totalCallsRecords[0].getValue('value');
             totalCallsRecords[0].setValue( ++totalCalls, "value");
           }
 
-          if( __status.toLowerCase() == 'Error'.toLowerCase() ) {
-            let errorsRecords = runtimeRecord.getLinkedRecords('errors', {before: 1});
-            if( errorsRecords && errorsRecords.length > 0
-                && errorsRecords[1]) {
-              let _totalErrors = errorsRecords[0].getValue('value');
-              let totalErrors = errorsRecords[1].getValue('value');
-              errorsRecords[1].setValue( ++totalErrors, 'value');
+          if( __status == 'ERROR' ) {
+            let errorsRecors = runtimeRecord.getLinkedRecords('errors', {before: 0});
+            if( errorsRecors && errorsRecors.length > 0
+                && errorsRecors[0]) {
+              let totalErrors = errorsRecors[0].getValue('value');
+              errorsRecors[0].setValue( ++totalErrors, 'value');
             }
 
           }
