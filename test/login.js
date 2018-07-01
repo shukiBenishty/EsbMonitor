b0f587f5379bac6311f915ba66a749fea70ed921
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-01 19:43:15 +0300

    Update login.js

--- test/login.js
+++ test/login.js
@@ -1,73 +1,65 @@
-var assert = require('assert'),
-//test = require('selenium-webdriver/testing'),
-webdriver = require('selenium-webdriver');
-import chai, { expect, should } from 'chai';
+import webdriver from 'selenium-webdriver';
 import SauceLabs from 'saucelabs';
 
 let driver;
 
-let username = 'olegkleiman' // process.env.SAUCE_USER;
-let accessKey = '6fbb18bd-5926-401d-9c6b-d50de081e752'; // process.env.SAUCE_KEY;
-let password = 'dfnc94^*';
-let testName = 'Sauce Login Test'
+const username = 'olegkleiman' // process.env.SAUCE_USER;
+const accessKey = '6fbb18bd-5926-401d-9c6b-d50de081e752'; // process.env.SAUCE_KEY;
+const password = 'dfnc94^*';
+const testName = 'Update Customer';
 
 let saucelabs = new SauceLabs({ username: username, password: password });
 
 describe('Login Tests', () => {
 
-   let sessionId;
+  let seessionId;
 
-   before( async() => {
+  before( async() => {
 
-     driver = await new webdriver.Builder()
-     .withCapabilities(webdriver.Capabilities.chrome())
-     //.usingServer("https://"+ username +":"+ accessKey +"@ondemand.saucelabs.com/wd/hub")
-     .build();
+    driver = await new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            //.usingServer("https://"+ username +":"+ accessKey +"@ondemand.saucelabs.com/wd/hub")
+            .build();
 
-     const session = await driver.getSession();
-     sessionId = session.id_;
-     console.log(`New sauce job to be created: ${sessionId}`);
+    const session = await driver.getSession();
+    seessionId = session.id_;
+    console.log(`Session id: ${seessionId}`);
 
-     await saucelabs.updateJob(sessionId, {
-       name: testName,
-       public: 'public',
-     });
-
-   })
-
-   it('Launch and Login', async() => {
-
-    //this.timeout(5000);
-
-    driver.get('http://mydigitel.tel-aviv.gov.il/');
-    // driver.manage().addCookie({
-    //   name: 'cookie-1',
-    //   value: 'cookieValue'
+    // await saucelabs.updateJob(sessionId, {
+    //   name: testName,
+    //   public: 'public',
     // });
-    // const loadedCookies = await driver.manage().getCookies();
-    // for(let cookie in loadedCookies) {
-    //   console.log(`Loaded cookie: ${cookie}`);
-    // }
 
+  })
+
+  it('Launch and Login', async() => {
+
+    //driver.get('http://mydigitel.tel-aviv.gov.il/');
+    driver.get('https://mydigitelpre13.tel-aviv.gov.il/Pages/ProfileUpdate.aspx');
 
     let elem = await driver.findElement(webdriver.By.name('UserName'));
-    elem.sendKeys("000000018");
+    elem.sendKeys('030614507');
 
-    elem = await driver.findElement(webdriver.By.name("Password"));
-    elem.sendKeys("test123");
+    elem = await driver.findElement(webdriver.By.name('Password'));
+    await elem.sendKeys('tyty1616');
 
-    elem = await driver.findElement(webdriver.By.css("span[class=submit]"));
+    elem = await driver.findElement(webdriver.By.css('span[class=submit]'));
     await elem.click();
     // Page be re-loaded after click(),
-    // so we need to await click() and re-enforce locators
+    // so we need to await click() and re-enforce locators    
 
-      //driver.wait(webdriver.until.elementLocated(webdriver.By.id("errorText")), 10000);
+     //driver.wait(webdriver.until.elementLocated(webdriver.By.id("errorText")), 10000);
+     // elem = await driver.findElement(webdriver.By.id("errorText"));
+     // const text = await elem.getText();
+     // console.log(`Error Text: ${text}`);
 
-      elem = await driver.findElement(webdriver.By.id("errorText"));
-      const text = await elem.getText();
-      console.log(`Error Text: ${text}`);
+    elem = await driver.findElement(webdriver.By.id('ctl00_ctl65_g_088dab0c_3a75_4d86_a248_4203f4a4c226_ctl00_txtEmail'));
+    await elem.clear();
+    await elem.sendKeys('oleg_kleyman@hotmail.com');
 
-  });
+    elem = await driver.findElement(webdriver.By.css('#ctl00_ctl65_g_088dab0c_3a75_4d86_a248_4203f4a4c226_ctl01_pnlButtons > a'));
+    await elem.click();
+  })
 
   after( async() => {
 
@@ -76,9 +68,10 @@ describe('Login Tests', () => {
     //   passed: true,
     // });
 
-    await driver.quit()
-    .then( () => console.log('DONE') )
-    .catch( (err) => console.error(err));
+    // await driver.quit()
+    // .then( () => console.log('QUIT') )
+    // .catch( (err) => console.log(err) );
 
   })
+
 });
