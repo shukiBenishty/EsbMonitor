51f70d079d210de8874d6ba33b443a5cc7f61d43
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-08 19:00:49 +0300

    Update Story.jsx

--- src/Story.jsx
+++ src/Story.jsx
@@ -1,11 +1,13 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import moment from 'moment';
 import Modal from 'react-modal';
 import elasticClient from '../elastic/connection';
 import esb from 'elastic-builder';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import Icon from './Icon';
+import settings from './settings.json';
 import 'react-vertical-timeline-component/style.min.css';
 
 import sampleStory from './SampleStory';
@@ -25,7 +27,8 @@ const customStyles = {
 type State = {
   modalIsOpen: boolean,
   events: [],
-  curentEventIndex: number
+  curentEventIndex: number,
+  serviceName: String
 }
 
 type Props = {
@@ -34,27 +37,35 @@ type Props = {
 
 class Story extends React.Component<Props, State> {
 
+  state = {
+    events: [],
+    modalIsOpen: false,
+    curentEventIndex: 0,
+    serviceName: ''
+  }
+
   constructor(props) {
 
     super(props);
 
-    const { match } = this.props;
+    //const { match } = this.props;
 
-    this.state = {
-      events: [],
-      modalIsOpen: false,
-      curentEventIndex: 0
-    }
+    // this.state = {
+    //   events: [],
+    //   modalIsOpen: false,
+    //   curentEventIndex: 0,
+    //   serviceName: ''
+    // }
 
-    this.styles = {
-      noDataStyle: {
-        width: '36%',
-        margin: '0 auto'
-      }
-    }
+    // this.styles = {
+    //   noDataStyle: {
+    //     width: '36%',
+    //     margin: '0 auto'
+    //   }
+    // }
 
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
+    //this.openModal = this.openModal.bind(this);
+    //this.closeModal = this.closeModal.bind(this);
     //this.afterOpenModal = this.afterOpenModal.bind(this);
   }
 
@@ -75,111 +86,185 @@ class Story extends React.Component<Props, State> {
     this.payload.textContent = this.state.events[this.state.curentEventIndex]._source.payload;
   }
 
-  componentDidMount() {
-    let storyId = this.props.match.params.storyId;
+  async getStoryEvents(storyId: number, rawIndexName: String) {
 
-    let requestBody = esb.requestBodySearch()
-    elasticClient.search({
-      index: 'esb_ppr_repository',
-      type: 'services',
-    }).then( response => {
+    try {
 
-    });
+      const requestBody = esb.requestBodySearch()
+      .query(
+        esb.matchQuery('message_guid',
+                       storyId)
+      )
+      .sort(esb.sort('start_date', 'asc'));
 
-    requestBody = esb.requestBodySearch()
-    .query(
-      esb.matchQuery('message_guid',
-                     storyId)
-    )
-    .sort(esb.sort('start_date', 'asc'));
-
-    const self = this;
-
-    elasticClient.search({
-        index: 'esb_ppr_row',
-        type: 'track',
-        body: requestBody.toJSON()
-    }).then( response => {
-      console.log(response);
+      const response = await elasticClient.search({
+          index: rawIndexName,
+          type: 'track',
+          body: requestBody.toJSON()
+      });
 
       if( response.hits.hits.length > 0  ) {
-        self.setState({
+
+        this.setState({
           events: response.hits.hits
         })
+
       } else {
-        self.setState({
+
+        this.setState({
           events: []
         })
+
       }
 
-    }).catch( error => {
-      console.error(error);
+    } catch( err ) {
+      console.error(err);
 
       // Only for tests purpose
       self.setState({
         events: sampleStory
       })
-    });
+
+    }
+
+  }
+
+  async getServiceName(storyId: number, summaryIndexName: String, servicesIndexName: String) {
+
+    try {
+
+      let requestBody = esb.requestBodySearch()
+      .query(
+        esb.matchQuery('message_guid',
+                       storyId)
+      );
+
+      let response = await elasticClient.search({
+        index: summaryIndexName,
+        type: 'summary',
+        body: requestBody.toJSON()
+      });
+
+      if( response.hits.hits.length == 0  ) {
+        console.error(`No results from ${summaryIndexName}`);
+        return;
+      }
+
+      const serviceId = response.hits.hits[0]._source.service_id;
+
+      requestBody = esb.requestBodySearch()
+      .query(
+        esb.matchQuery('service_id',
+                       serviceId)
+      );
+      response = await elasticClient.search({
+         index: servicesIndexName,
+         body: requestBody.toJSON()
+      });
+
+      if( response.hits.hits.length == 0  ) {
+        console.error(`No results from ${servicesIndexName}`);
+        return;
+      }
+
+      this.setState({
+        serviceName: response.hits.hits[0]._source.name
+      });
+
+    } catch( err ){
+      console.error(err);
+    }
+
+  }
+
+   componentDidMount() {
+
+    let storyId = this.props.match.params.storyId;
+
+    const rawIndexName = settings[this.props.environment].raw_index_name;
+    const summaryIndexName = settings[this.props.environment].summary_index_name;
+    const servicesIndexName = settings[this.props.environment].services_index_name;
+    console.log(`Using ${rawIndexName} index`);
+
+    // Execute search query against Raw index to get full story details
+    // This is asynchronous invocation
+    ::this.getStoryEvents(storyId, rawIndexName);
+    // Execute almost (except order) same search query
+    // against Summary index to get the details for caption
+    //
+    // This is also an asynchronous invocation
+    ::this.getServiceName(storyId, summaryIndexName, servicesIndexName);
+
   }
 
   render() {
 
     if( this.state.events.length == 0 ) {
-      return (<h4 style={this.styles.noDataStyle}>No data was collected for this invocation</h4>
+      return (<h4 className='noData'>No data was collected for this invocation</h4>
 
               )
     }
 
     return (
-      <VerticalTimeline>
-        <Modal
-           style={customStyles}
-           onRequestClose={this.closeModal}
-           onAfterOpen={::this.afterOpenModal}
-           isOpen={this.state.modalIsOpen}
-           ariaHideApp={false}
-           currentEventIndex = {1}
-           contentLabel="Payload">
-           <h2>Payload</h2>
-           <div ref={ div => this.payload = div }>Message</div>
-        </Modal>
+      <React.Fragment>
+        <h2 className='serviceNameCaption'>Flow of {this.state.serviceName}</h2>
+        <h3 className='serviceNameCaption'>({this.props.match.params.storyId})</h3>
+        <VerticalTimeline>
+          <Modal
+             style={customStyles}
+             onRequestClose={::this.closeModal}
+             onAfterOpen={::this.afterOpenModal}
+             isOpen={this.state.modalIsOpen}
+             ariaHideApp={false}
+             currentEventIndex = {1}
+             contentLabel="Payload">
+             <h2>Payload</h2>
+             <div ref={ div => this.payload = div }>Message</div>
+          </Modal>
 
-        {
+          {
 
-          this.state.events.map( (esbEvent, index) => {
+            this.state.events.map( (esbEvent, index) => {
 
-            let iconStyle = ( esbEvent._source.status == 'success' ) ?
-                              { background: 'rgb(33, 150, 243)', color: '#fff' } :
-                              { background: 'rgb(233, 30, 99)', color: '#fff' };
+              let iconStyle = ( esbEvent._source.status == 'success' ) ?
+                                { background: 'rgb(33, 150, 243)', color: '#fff' } :
+                                { background: 'rgb(233, 30, 99)', color: '#fff' };
 
-            let iconType = ( esbEvent._source.status == 'success' )  ?
-                            'icon ti-info' :
-                            'icon ti-alert';
+              let iconType = ( esbEvent._source.status == 'success' )  ?
+                              'icon ti-info' :
+                              'icon ti-alert';
 
-            let latency = moment.duration(moment(esbEvent._source.end_date).diff(moment(esbEvent._source.start_date)));
-            let environment = ( esbEvent._source.environment == 'INT1' ) ? 'External' : 'Internal';
+              let latency = moment.duration(moment(esbEvent._source.end_date).diff(moment(esbEvent._source.start_date)));
+              let environment = ( esbEvent._source.environment == 'INT1' ) ? 'External' : 'Internal';
 
-            return <VerticalTimelineElement key={index}
-                      className="vertical-timeline-element"
-                      date={moment(esbEvent._source.start_date).format('DD/MM/YYYY, h:mm:ss.SS')}
-                      iconStyle={iconStyle}
-                      icon={<Icon type={iconType}/>}
-                      >
-                      <h3 className="vertical-timeline-element-title"><b>{esbEvent._source.message}</b></h3>
-                      <h4 className="vertical-timeline-element-subtitle"><b>by Router at {environment} environment</b></h4>
-                      <div>From {esbEvent._source.client_ip}</div>
-                      <div>User {esbEvent._source.client_user}</div>
-                      <p>
-                        <button className='btn'
-                              onClick={ () => this.openModal(index) }>Payload</button>
-                      </p>
-                   </VerticalTimelineElement>
-          })
-        }
-      </VerticalTimeline>
+              return <VerticalTimelineElement key={index}
+                        className="vertical-timeline-element"
+                        date={moment(esbEvent._source.start_date).format('DD/MM/YYYY, hh:mm:ss.SS')}
+                        iconStyle={iconStyle}
+                        icon={<Icon type={iconType}/>}
+                        >
+                        <h3 className="vertical-timeline-element-title"><b>{esbEvent._source.message}</b></h3>
+                        <h4 className="vertical-timeline-element-subtitle"><b>by Router at {environment} environment</b></h4>
+                        <div>From {esbEvent._source.client_ip}</div>
+                        <div>User {esbEvent._source.client_user}</div>
+                        <p>
+                          <button className='btn'
+                                onClick={ () => ::this.openModal(index) }>Payload</button>
+                        </p>
+                     </VerticalTimelineElement>
+            })
+          }
+        </VerticalTimeline>
+     </React.Fragment>
     )
   }
 
 }
 
-export default Story;
+const mapStateToProps = state => {
+  return {
+    environment: state.activeEnvironment
+  }
+}
+
+export default connect(mapStateToProps)(Story);
