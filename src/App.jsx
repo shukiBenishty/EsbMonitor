7c4de13b45491929429ec86c2efaaed74ead3cc4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-10 17:50:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,15 @@ const realtimeEventsSubscription = graphql`
   }
 `;
 
-class AppLayout extends React.Component {
+type Props = {
+  activeTracing: Boolean
+}
+
+class AppLayout extends React.Component<Props> {
+
+  static defaultProps = {
+    activTracing: true
+  }
 
   publishEsbEvent = (payload) => {
 
@@ -38,87 +46,99 @@ class AppLayout extends React.Component {
 
   }
 
-  componentDidMount() {
+  subscriptionConfig = {
+    subscription: realtimeEventsSubscription,
+    variables: {},
+    onNext: payload => {
 
-    const self = this;
+      // TBD: Temporay solution: change this to use Relay fragment!
+      this.publishEsbEvent(payload.traceAdded);
 
-    const subscriptionConfig = {
-      subscription: realtimeEventsSubscription,
-      variables: {},
-      onNext: payload => {
+    },
+    updater: proxyStore => {
 
-        // TBD: Temporay solution: change this to use Relay fragment!
-        self.publishEsbEvent(payload.traceAdded);
+      //  Reading values off the Payload
+      const rootField = proxyStore.getRootField('traceAdded');
+      const __type = rootField.getType();
+      const __status = rootField.getValue('status');
+      const __serviceId = rootField.getValue('serviceId');
 
-      },
-      updater: proxyStore => {
+      // Reading Values off the Relay Store
+      let root = proxyStore.getRoot();
+      let _type = root.getType();
+      let runtimeRecord = root.getLinkedRecord('runtime');
+      if( runtimeRecord ) {
 
-        //  Reading values off the Payload
-        const rootField = proxyStore.getRootField('traceAdded');
-        const __type = rootField.getType();
-        const __status = rootField.getValue('status');
-        const __serviceId = rootField.getValue('serviceId');
+        let distributionRecord =
+          runtimeRecord.getOrCreateLinkedRecord('distribution', 'Series',
+                                                {daysBefore: 0, servicesIds: [1,3]});
 
-        // Reading Values off the Relay Store
-        let root = proxyStore.getRoot();
-        let _type = root.getType();
-        let runtimeRecord = root.getLinkedRecord('runtime');
-        if( runtimeRecord ) {
-
-          let distributionRecord =
-            runtimeRecord.getOrCreateLinkedRecord('distribution', 'Series',
-                                                  {daysBefore: 0, servicesIds: [1,3]});
-
-          if( distributionRecord ) {
-              let seriesRecords = distributionRecord.getLinkedRecords('series');
-              if( seriesRecords ) {
-                  for(let i = 0; i < seriesRecords.length; i++) {
-                      let serviceId = seriesRecords[i].getValue('serviceId');
-                      if( serviceId == __serviceId ) {
-                         let data = seriesRecords[i].getValue('data');
-                         let _data =   _.map(data, _.clone); // clone data in order to be able to change it
-                         if( !_data[0] ) { // in case the 'distrubution' record was just created
-                            _data[0] = 1; // consider today ([0])
-                            for(let j = 1; j < daysBefore; j++) { // fill the rest
-                              _data[j] = 0;
-                            }
-                         } else {
-                           _data[0] = data[0] + 1;
-                         }
-                         seriesRecords[i].setValue(_data, 'data');
-                      }
-                  }
-              }
-          }
-
-          let totalCallsRecords = runtimeRecord.getLinkedRecords('totalCalls', {before: 1});
-          if( totalCallsRecords && totalCallsRecords.length > 0
-              && totalCallsRecords[0] ) {
-            let totalCalls = totalCallsRecords[0].getValue('value');
-            totalCallsRecords[0].setValue( ++totalCalls, "value");
-          }
-
-          if( __status == 'ERROR' ) {
-            let errorsRecors = runtimeRecord.getLinkedRecords('errors', {before: 1});
-            if( errorsRecors && errorsRecors.length > 0
-                && errorsRecors[0]) {
-              let totalErrors = errorsRecors[0].getValue('value');
-              errorsRecors[0].setValue( ++totalErrors, 'value');
+        if( distributionRecord ) {
+            let seriesRecords = distributionRecord.getLinkedRecords('series');
+            if( seriesRecords ) {
+                for(let i = 0; i < seriesRecords.length; i++) {
+                    let serviceId = seriesRecords[i].getValue('serviceId');
+                    if( serviceId == __serviceId ) {
+                       let data = seriesRecords[i].getValue('data');
+                       let _data =   _.map(data, _.clone); // clone data in order to be able to change it
+                       if( !_data[0] ) { // in case the 'distrubution' record was just created
+                          _data[0] = 1; // consider today ([0])
+                          for(let j = 1; j < daysBefore; j++) { // fill the rest
+                            _data[j] = 0;
+                          }
+                       } else {
+                         _data[0] = data[0] + 1;
+                       }
+                       seriesRecords[i].setValue(_data, 'data');
+                    }
+                }
             }
-
-          }
         }
-      },
-      onError: error => {
-        console.log(`An error occured:`, error);
+
+        let totalCallsRecords = runtimeRecord.getLinkedRecords('totalCalls', {before: 1});
+        if( totalCallsRecords && totalCallsRecords.length > 0
+            && totalCallsRecords[0] ) {
+          let totalCalls = totalCallsRecords[0].getValue('value');
+          totalCallsRecords[0].setValue( ++totalCalls, "value");
+        }
+
+        if( __status == 'ERROR' ) {
+          let errorsRecors = runtimeRecord.getLinkedRecords('errors', {before: 1});
+          if( errorsRecors && errorsRecors.length > 0
+              && errorsRecors[0]) {
+            let totalErrors = errorsRecors[0].getValue('value');
+            errorsRecors[0].setValue( ++totalErrors, 'value');
+          }
+
+        }
       }
-    };
+    },
+    onError: error => {
+      console.log(`An error occured:`, error);
+    }
+  }
+
+  componentDidMount() {
 
     this.subscription = requestSubscription(
       environment,
-      subscriptionConfig
+      this.subscriptionConfig
     )
 
+  }
+
+  componentDidUpdate(prevProps) {
+    // The only property of interest here is activeTracking
+    if( prevProps.activeTracing != this.props.activeTracing ){
+        if( !this.props.activeTracing  ) {
+          this.subscription.dispose();
+        this.subscription = null;
+        } else {
+
+          // this.subscription = requestSubscription(environment,
+          //                                         this.subscriptionConfig)
+        }
+    }
   }
 
   render() {
@@ -129,6 +149,12 @@ class AppLayout extends React.Component {
             </div>);
   }
 
-};
+}
 
-export default withRouter(connect()(AppLayout))
+const mapStateToProps = state => {
+ return {
+  activeTracing: state.activeTracing
+ }
+}
+
+export default withRouter(connect(mapStateToProps)(AppLayout))
